648ccf9c11b4b9015ca8ab79613d59a969b2667f
jamesmcculloch1 <jmcculloch545@gmail.com>
2021-09-16 03:11:30 +0800

    fixed cors bug with socket.io

---
+++ messagingapp/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+//13mins in https://www.youtube.com/watch?v=tBr-PybP_9c
